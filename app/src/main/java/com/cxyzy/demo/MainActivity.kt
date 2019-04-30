bd3a62d4558803cc9b60b3c3c42629abf75b49fc
jerry <309032663@qq.com>
2019-04-30 09:39:31 +0800

    update

--- app/src/main/java/com/cxyzy/demo/MainActivity.kt
+++ app/src/main/java/com/cxyzy/demo/MainActivity.kt
@@ -3,8 +3,6 @@ package com.cxyzy.demo
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sunfusheng.marqueeview.MarqueeView
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,10 +10,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val messages = ArrayList<String>()
+        val messages = mutableListOf<String>()
         messages.add("《赋得古原草送别》")
         messages.add("离离原上草，一岁一枯荣。")
         messages.add("野火烧不尽，春风吹又生。")
+        val marqueeView = findViewById<MarqueeView<String>>(R.id.marqueeView)
         marqueeView.setFlipInterval(1000)
         marqueeView.startWithList(messages)
     }
